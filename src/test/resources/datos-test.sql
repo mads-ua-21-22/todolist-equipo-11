3edc07e7d6f261ac5d58682b07d64204694a6b45
Dario Guerrero Montero <dgm94@clL15-7>
2021-10-27 14:26:24 +0200

    Added perfiles para trabajar con postgres

--- src/test/resources/datos-test.sql
+++ src/test/resources/datos-test.sql
@@ -1,4 +1,4 @@
 /* Populate tables */
-INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento) VALUES('1', 'user@ua', 'Usuario Ejemplo', '123', '2001-02-10');
-INSERT INTO tareas (id, titulo, usuario_id) VALUES('1', 'Lavar coche', '1');
-INSERT INTO tareas (id, titulo, usuario_id) VALUES('2', 'Renovar DNI', '1');
+INSERT INTO usuarios (email, nombre, password, fecha_nacimiento) VALUES('user@ua', 'Usuario Ejemplo', '123', '2001-02-10');
+INSERT INTO tareas (titulo, usuario_id) VALUES('Lavar coche', '1');
+INSERT INTO tareas (titulo, usuario_id) VALUES('Renovar DNI', '1');
