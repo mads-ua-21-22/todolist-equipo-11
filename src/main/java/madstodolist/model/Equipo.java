77a9cea58b10ad028611d10fde1cfa0268384b26
Dario Guerrero <dgm94@alu.ua.es>
2021-11-06 12:57:07 +0100

    Lo justo para TEST1

---
+++ src/main/java/madstodolist/model/Equipo.java
@@ -0,0 +1,46 @@
+package madstodolist.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Equipo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String nombre;
+
+    // Constructor público con los atributos obligatorios. En este caso el correo electrónico.
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Equipo() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Equipo equipo = (Equipo) o;
+        return Objects.equals(id, equipo.id);
+    }
+
+}
