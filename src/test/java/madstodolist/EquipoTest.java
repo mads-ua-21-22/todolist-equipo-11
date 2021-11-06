b66ecaec489177fca82789c5ebee066715ea7945
Dario Guerrero <dgm94@alu.ua.es>
2021-11-06 12:27:14 +0100

    Creacion de Equipo, TEST1

---
+++ src/test/java/madstodolist/EquipoTest.java
@@ -0,0 +1,18 @@
+package madstodolist;
+
+// imports
+
+import madstodolist.model.Equipo;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class EquipoTest {
+
+    @Test
+    public void crearEquipo() {
+        Equipo equipo = new Equipo("Proyecto P1");
+        assertThat(equipo.getNombre()).isEqualTo("Proyecto P1");
+    }
+}
