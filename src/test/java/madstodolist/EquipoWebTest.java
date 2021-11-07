8d1243543f0fea84169a70642b72362643aec3b2
Dario Guerrero <dgm94@alu.ua.es>
2021-11-07 12:01:21 +0100

    Test 1 - Obtener vista de listado de equipos

---
+++ src/test/java/madstodolist/EquipoWebTest.java
@@ -0,0 +1,34 @@
+package madstodolist;
+
+import madstodolist.model.Usuario;
+import madstodolist.service.UsuarioService;
+import madstodolist.service.UsuarioServiceException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EquipoWebTest {
+    @Autowired
+    private MockMvc mockMvc;
+    UsuarioService usuarioService;
+
+    @Test
+    public void listadoDeEquipos() throws Exception {
+        this.mockMvc.perform(get("/equipos"))
+                .andExpect(content().string(containsString("Equipos")));
+    }
+}
